b79dd39e212654cab423a5d02a460e311374540d
zdluoa <835825423@qq.com>
2017-11-10 00:00:57 +0800

    跳转逻辑优化

--- manager-system-front/main.js
+++ manager-system-front/main.js
@@ -13,17 +13,19 @@ angular.module('manager.system.demo', ['ui.router', 'manager.system.business'])
     .controller('mainController', ['$scope', '$http', '$state', 'cookieService',
      ($scope, $http, $state, cookieService)=>{
         if(!cookieService.getToken()){
-            $state.go('login');
+            $state.transitionTo('login');
+        } else {
+            $state.transitionTo('home');
         }
     }])
     .run(['$rootScope', '$state', 'cookieService', function($rootScope, $state, cookieService){
         $rootScope.$on('$stateChangeStart', 
             function(evt, toState, roParams, fromState, fromParams) {
                 if(!cookieService.getToken() && toState.name != 'login') {
-                    $state.go('login');
+                    $state.transitionTo('login');
                     evt.preventDefault();
                 } else if(cookieService.getToken() && toState.name == 'login'){
-                    $state.go('home');
+                    $state.transitionTo('home');
                     evt.preventDefault();
                 }
             });
