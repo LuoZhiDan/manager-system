7f1611b7da7dc08408906dc3d6ca283f14c76658
zdluoa <835825423@qq.com>
2017-11-16 23:16:27 +0800

    首页开发

--- manager-system-front/src/frame/top-nav/controller/top-controller.js
+++ manager-system-front/src/frame/top-nav/controller/top-controller.js
@@ -1,12 +1,12 @@
-export default ['$scope', 'cookieService', '$state', function($scope, cookieService, $state){
-    $scope.topNavURL = 'frame/top-nav/view/top-view.html';
-
-
-
-    $scope.action = {
-        loginOut : ()=>{
-            cookieService.loginOut();
-            $state.transitionTo('login');
+export default ['$scope', 'cookieService', '$state', 
+    function($scope, cookieService, $state){
+    
+        $scope.topNavURL = 'frame/top-nav/view/top-view.html';
+        
+        $scope.action = {
+            loginOut : ()=>{
+                cookieService.loginOut();
+                $state.transitionTo('login');
+            }
         }
-    }
 }];
